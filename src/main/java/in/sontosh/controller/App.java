a5bb88a6e7abca4b70cd1ecb9a76349efb061f30
gcc <gcc@DESKTOP-4F1E50B>
2020-10-18 07:58:11 +0530

    sk-27 -Modifying

--- src/main/java/in/sontosh/controller/App.java
+++ src/main/java/in/sontosh/controller/App.java
@@ -1,6 +1,16 @@
 package in.sontosh.controller;
- 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
-public class App { 
+@RequestMapping("/sk")
+public class App {
+
+	@GetMapping("/wel")
+	private String welCome() {
+		return "Wel Come To GitHub!!";
+	}
 
 }
